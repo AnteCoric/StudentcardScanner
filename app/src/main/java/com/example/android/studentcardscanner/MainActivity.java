9a1086375211f001bfd4a6780849d31cc9bb273e
ante coric <antecoric@hotmail.be>
2018-12-04 11:58:58 +0100

    First commit

---
+++ app/src/main/java/com/example/android/studentcardscanner/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.studentcardscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    static public boolean test = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
