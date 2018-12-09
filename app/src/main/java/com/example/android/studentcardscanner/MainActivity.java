feb5908d306b100ee229fce9c35cf71718053c7a
Pepijn Janssen <pepijn.janssen@hotmail.com>
2018-12-09 19:18:21 +0100

    new commit

--- app/src/main/java/com/example/android/studentcardscanner/MainActivity.java
+++ app/src/main/java/com/example/android/studentcardscanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.studentcardscanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchNFC(View view) {
-
+        Intent intent = new Intent(this, secondActivity.class);
+        //mogelijke informatie dat ook door gegeven moet worden hier zetten in code (to future self)
+        startActivity(intent);
     }
 }
