67fa60998890be1066e917f30fd9c82e2567b889
Pepijn Janssen <pepijn.janssen@hotmail.com>
2018-12-11 10:07:49 +0100

    nfc read (i think)

--- app/src/main/java/com/example/android/studentcardscanner/secondActivity.java
+++ app/src/main/java/com/example/android/studentcardscanner/secondActivity.java
@@ -1,10 +1,16 @@
 package com.example.android.studentcardscanner;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class secondActivity extends AppCompatActivity {
+
+    private String DataPayload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,26 @@ public class secondActivity extends AppCompatActivity {
     }
 
     public void nfcStart(View view) {
+        onNewIntent();//nog een intent toevoegen
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    //...
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for (int i = 0; i < rawMessages.length; i++) {
+                    messages[i] = (NdefMessage) rawMessages[i];
+                    DataPayload += messages[i];
+                }
+                // Process the messages array.
+            //...
+            }
+        }
     }
 }
