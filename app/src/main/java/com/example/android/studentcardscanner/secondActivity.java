5ffefafd5ccda7b356ff82bdeb88003b272a88b3
Pepijn Janssen <pepijn.janssen@hotmail.com>
2018-12-11 10:45:08 +0100

    nfc check

--- app/src/main/java/com/example/android/studentcardscanner/secondActivity.java
+++ app/src/main/java/com/example/android/studentcardscanner/secondActivity.java
@@ -7,20 +7,36 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class secondActivity extends AppCompatActivity {
 
+    NfcAdapter mNfcAdapter;
     private String DataPayload;
+    private TextView mInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // Check for available NFC Adapter
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        mInfo = (TextView) findViewById(R.id.info);
     }
 
     public void nfcStart(View view) {
-        onNewIntent();//nog een intent toevoegen
+        mInfo.setText("geting data");
+        //onNewIntent();//nog een intent toevoegen
 
+        mInfo.setText(DataPayload);
     }
 
     @Override
